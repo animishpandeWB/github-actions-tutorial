a5e38b2076573830330f9cdd43f7a4dcfadfc782
Animish Pande <137478828+animishpandeWB@users.noreply.github.com>
2025-04-23 12:29:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def hello():
-  print("hi from github actions!")
+    print("hi from github actions!")
+
 
 def bye():
-  print("Bye from github actions!")
+    print("Bye from github actions!")
 
 print(hello())
